977dc9315bef17f9c16e457dda0ba5a3ffe6b3da
Mike Dent <mikedent85@gmail.com>
2021-02-23 11:14:17 -0500

    config changes for deployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const api = {
   API_ENDPOINT: 'http://localhost:8000/api',
   API_KEY: process.env.REACT_APP_API_KEY,
   NBA_API_ENDPOINT: 'https://www.balldontlie.io/api/v1/players',
-  TOKEN_KEY: 'change-this-secret',
+  TOKEN_KEY: process.env.TOKEN_KEY,
 }
 
 export default api
